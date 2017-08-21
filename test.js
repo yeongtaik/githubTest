a053b7b245fc1e9ac4be40a02535e42a808faab4
yeongtaik byeon <yt.byeon@samsung.com>
2017-08-21 14:04:29 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,11 +1,5 @@
 
-function CWE_398() { // IDENTICAL_BRANCHES
-    if (x >= 0) {
-        y = x;
-    } else {
-        y = x;
-    }
-}
+
 function CWE_476() { // NULL_POINTER
     var obj;
     var y = obj.x;
