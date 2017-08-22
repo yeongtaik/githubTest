792760187d1a43aeb1ace29a9f367f5335c3464c
yeongtaik byeon <yt.byeon@samsung.com>
2017-08-22 11:05:56 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,15 +1,4 @@
-function CWE_398() { // IDENTICAL_BRANCHES
-    if (x >= 0) {
-        y = x;
-    } else {
-        y = x;
-    }
-}
-function CWE_476() { // NULL_POINTER
-    var obj;
-    var y = obj.x;
-    console.log(y);
-}
+
 
 function CWE_476() { // NULL_POINTER
     var obj;
