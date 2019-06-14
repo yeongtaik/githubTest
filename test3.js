27abe2e05fee4568c5a1deea1b262dfa74fd70cc
yeongtaik2 <yeongtaik@gmail.com>
2019-06-14 12:05:25 +0900

    auto analysis test

--- test3.js
+++ test3.js
@@ -26,3 +26,16 @@ define(['dijit',
         'webida-lib/util/path',
         './dlg-content-template'],
 });
+
+function CWE_398() { // IDENTICAL_BRANCHES
+    if (x >= 0) {
+        y = x;
+    } else {
+        y = x;
+    }
+}
+function CWE_476() { // NULL_POINTER
+    var obj;
+    var y = obj.x;  // UNINITIALIZED_LOCAL_VAR
+    console.log(y);
+}
